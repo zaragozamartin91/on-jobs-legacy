8b1e2c7ecb63e92435960498cecdb341820b1ae7
zaragozamartin91 <zaragozamartin91@gmail.com>
2019-02-02 16:03:07 -0300

    Se termino con el camino feliz de la creacion de un aviso, pero sin la publicacion del mismo.

--- src/test/java/com/on/jobs/publica/sbe/CrearYPublicarAvisoTest.java
+++ src/test/java/com/on/jobs/publica/sbe/CrearYPublicarAvisoTest.java
@@ -10,6 +10,7 @@ import cucumber.deps.com.thoughtworks.xstream.InitializationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -24,9 +25,13 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
     //    private static final String URL = "http://www.bumeran.com.ar/api/publicador/index.bum";
@@ -43,6 +48,7 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
     /* Este codigo se debe tomar del catalogo "IDPLANPUBLICACION" de acuerdo al tipo de membresia y pais contratado. */
     private static final String OK_PRODUCT_ID = "60";
     private static final String CHARSET = "UTF-8";
+    private static final String TXP_TEXT = "Cajero /a Unico /a Eventual";
 
     private String docToString(Document document) {
         try {
@@ -70,23 +76,36 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
         }
     }
 
+    private Document parseDocument(String xmlString) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(new InputSource(new StringReader(xmlString)));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error al parsear documento " + xmlString, e);
+        }
+    }
 
     private Document document;
 
 
-    private Element getDocumentElement() {
+    private Element getDocumentElement(Document doc) {
         /* TODO : analizar si es necesario normalizar el documento antes de enviarlo */
         //        documentElement.normalize();
-        return document.getDocumentElement();
+        return doc.getDocumentElement();
     }
 
-    private Element getElement(String path) {
+    private Element getElement(String path, Document doc) {
         try {
             XPathExpression expression = xpath.compile(path);
-            return (Element) expression.evaluate(getDocumentElement(), XPathConstants.NODE);
+            return (Element) expression.evaluate(getDocumentElement(doc), XPathConstants.NODE);
         } catch (XPathExpressionException e) {
             throw new IllegalArgumentException("Expresion " + path + " invalida", e);
         }
+    }
+
+    private Element getElement(String path) {
+        return getElement(path, document);
     }
 
     /**
@@ -118,7 +137,7 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
 
     @Dado("un formulario de aviso con sus campos requeridos completados correctamente")
     public void un_formulario_de_aviso_con_sus_campos_requeridos_completados_correctamente() throws IOException, URISyntaxException {
-        getElement("/Avisos/aviso/txPuesto").setTextContent("Cajero /a Unico /a Eventual");
+        getElement("/Avisos/aviso/txPuesto").setTextContent(TXP_TEXT);
 
         getElement("/Avisos/aviso/txDescripcion")
                 .setTextContent("<![CDATA[<p>Si te interesa formar parte de un equipo de trabajo agradable, te invitamos a integrarte a Empresa S.A.</p>]]>");
@@ -148,7 +167,13 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
                 .execute();
 
 
-        System.out.println(response.getBody().get());
+        String responseBody = response.getBody().get();
+        System.out.println(responseBody);
+
+        Document responseDoc = parseDocument(responseBody);
+        Element responseTxp = getElement("/Retorno/aviso/Aviso", responseDoc);
+        assertNotNull(responseTxp);
+        assertEquals(TXP_TEXT, responseTxp.getTextContent());
     }
 
     /* Fin de Escenario: crear y publicar un aviso correctamente ------------------------------- */
@@ -157,30 +182,36 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
 
     @Dado("un codigoAviso duplicado para dicho integrador")
     public void un_codigoAviso_duplicado_para_dicho_integrador() {
+        System.out.println("un_codigoAviso_duplicado_para_dicho_integrador");
     }
 
     @Entonces("la creacion del aviso falla por codigoAviso duplicado")
     public void la_creacion_del_aviso_falla_por_codigoAviso_duplicado() throws Exception {
+        System.out.println("la_creacion_del_aviso_falla_por_codigoAviso_duplicado");
     }
 
     /* Fin de crear y publicar un aviso falla por duplicacion de codigoAviso ------------------ */
 
     @Dado("un formulario de aviso con sus campos requeridos completados erroneamente")
     public void un_formulario_de_aviso_con_sus_campos_requeridos_completados_erroneamente() throws IOException {
+        System.out.println("un_formulario_de_aviso_con_sus_campos_requeridos_completados_erroneamente");
     }
 
     @Entonces("la creacion del aviso falla por errores presentes en el formulario de aviso")
     public void la_creacion_del_aviso_falla_por_errores_presentes_en_el_formulario_de_aviso() throws Exception {
+        System.out.println("la_creacion_del_aviso_falla_por_errores_presentes_en_el_formulario_de_aviso");
     }
 
     /* Escenario: crear y publicar un aviso falla porque la empresa no tiene creditos suficientes para publicarlo */
 
     @Dado("que tiene creditos insuficientes para publicar avisos")
     public void que_tiene_creditos_insuficientes_para_publicar_avisos() {
+        System.out.println("que_tiene_creditos_insuficientes_para_publicar_avisos");
     }
 
     @Entonces("la publicacion del aviso falla por falta de creditos de la empresa")
     public void la_publicacion_del_aviso_falla_por_falta_de_creditos_de_la_empresa() throws Exception {
+        System.out.println("la_publicacion_del_aviso_falla_por_falta_de_creditos_de_la_empresa");
     }
 
     /* fin de crear y publicar un aviso falla porque la empresa no tiene creditos suficientes para publicarlo */
@@ -189,10 +220,13 @@ public class CrearYPublicarAvisoTest extends SpringIntegrationTest {
 
     @Dado("una empresa no habilitada para integrar")
     public void una_empresa_no_habilitada_para_integrar() {
+        document = buildDocument();
+        System.out.println("una_empresa_no_habilitada_para_integrar");
     }
 
     @Entonces("la creacion del aviso falla porque la empresa no esta habilitada para integrar")
     public void la_creacion_del_aviso_falla_porque_la_empresa_no_esta_habilitada_para_integrar() throws Exception {
+        System.out.println("la_creacion_del_aviso_falla_porque_la_empresa_no_esta_habilitada_para_integrar");
     }
 
     /* fin de crear y publicar un aviso falla porque la empresa no esta habilitada para integrar */
